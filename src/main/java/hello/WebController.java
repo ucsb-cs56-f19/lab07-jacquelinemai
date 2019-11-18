1cabd389c7a839fe00af82b60afd24ed30b26cde
Jacqueline Mai <jacquelinemai@umail.ucsb.edu>
2019-11-17 22:18:15 -0800

    jm - Added placeholder for eq query service and placeholder on results form

--- src/main/java/hello/WebController.java
+++ src/main/java/hello/WebController.java
@@ -55,10 +55,14 @@ public class WebController {
 	return "earthquakes/search";
     }
 
-     @GetMapping("/earthquakes/results")
+    @GetMapping("/earthquakes/results")
     public String getEarthquakesResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken, EqSearch eqSearch) {
-        model.addAttribute("eqSearch", eqSearch);
-        // TODO: Actually do the search here and add results to the model
+        EarthquakeQueryService e = new EarthquakeQueryService();
+	model.addAttribute("eqSearch", eqSearch);
+        String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag());
+	model.addAttribute("json", json);
         return "earthquakes/results";
     }
+
+
 }
